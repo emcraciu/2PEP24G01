b82edafa34e0006dceb053426332ff4318bebd8e
emcraciu <ozy24us@gmail.com>
2024-04-22 20:02:54 +0300

    app1

--- modul1/app1.py
+++ modul1/app1.py
@@ -1,9 +1,26 @@
 """Decorator to execute function only during working hours"""
 
+from functools import wraps
+import time
+
+
 # decorator
 def working_hours(start: int, stop: int):
-    pass
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            local_time = time.localtime().tm_hour
+            if start < local_time < stop:
+                func(*args, **kwargs)
 
-@working_hours(9, 22)
+        return wrapper
+
+    return decorator
+
+
+@working_hours(9, 20)
 def alarm():
-    print('Wake UP!!!!')
+    print("Wake up!")
+
+
+alarm()
