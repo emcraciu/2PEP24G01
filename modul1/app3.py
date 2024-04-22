7a62e2eebad50dbc56b1640527a08b415f890fef
emcraciu <ozy24us@gmail.com>
2024-04-22 21:46:13 +0300

    app4

--- modul1/app3.py
+++ modul1/app3.py
@@ -1,8 +1,24 @@
 """Createdecorator for adding customisable delay in function resonse """
 
+from time import sleep
+from functools import wraps
+
+
 def delay(seconds=0):
-    pass
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            sleep(seconds)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
 
 @delay(seconds=5)
 def area(length: int, width: int):
     return length * width
+
+
+print(area(10, 10))
