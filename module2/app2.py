2a7e0bf18db3cb555971044ede2c3ab1550bd563
emcraciu <ozy24us@gmail.com>
2024-04-24 21:19:30 +0300

    part2

--- module2/app2.py
+++ module2/app2.py
@@ -1,6 +1,9 @@
 import os
 import re
+import time
+
 from module2.app1 import get_interface_ip
+
 
 class SystemInformation():
 
@@ -9,7 +12,22 @@ class SystemInformation():
         return get_interface_ip(ip_data)
 
     def get_cpu_average(self, interval):
+        start_time = time.time()
+        total = 0
+        count = 0
+        while time.time() <= start_time + interval:
+            cpu_usage_output = os.popen('wmic cpu get loadpercentage')
+            pattern = re.compile(r"(?P<cpu>\d{1,3})")
+            cpu_group = pattern.search(cpu_usage_output.read())
+            number = int(cpu_group.group('cpu'))
+            total += number
+            count += 1
+        average = total / count
         return average
 
+    def get_route_information(self):
+        pass
 
 
+system = SystemInformation()
+print(system.get_cpu_average(7))
