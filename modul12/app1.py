d0919652cb675cbc9a8831990b6d87d93be7821d
emcraciu <ozy24us@gmail.com>
2024-06-12 20:10:33 +0300

    modul11

---
+++ modul12/app1.py
@@ -0,0 +1,3 @@
+from modul12.part2 import compare_factorials
+
+compare_factorials()
--- modul12/part2.py
+++ modul12/part2.py
@@ -1,3 +1,6 @@
+import timeit
+
+
 def factorial1(n):
     result = 1
     for i in range(1, n + 1):
@@ -12,4 +15,14 @@ def factorial2(n):
         return n * factorial2(n - 1)
 
 
+x = list(range(1, 255))
+factorial1_values = []
+factorial2_values = []
 
+
+def compare_factorials():
+    for i in range(1, 255):
+        rez = timeit.timeit(f'factorial1({i})', setup=f"from {__name__} import factorial1", number=100)
+        factorial1_values.append(rez)
+
+    print(factorial1_values)
