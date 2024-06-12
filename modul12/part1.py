a742583ec23b37afeb349d55d7e784a10fa6ce70
emcraciu <ozy24us@gmail.com>
2024-06-12 20:08:20 +0300

    modul11

--- modul12/part1.py
+++ modul12/part1.py
@@ -4,8 +4,6 @@ from random import randint
 import matplotlib.pyplot as plt
 import math
 
-math.l
-
 
 def random_gen(num_iter):
     result = []
@@ -27,7 +25,7 @@ for i in range(1000, 3000, 100):
 fig, (area1, area2) = plt.subplots(nrows=2, ncols=1, sharex="all")
 fig.dpi = 200
 area1.plot(x, y, label="Time take to generate numbers")
-area2.plot(x, y, label="Time take to generate numbers")
+area2.plot(x, list(map(lambda y1: math.log(y1), y)), label="Time take to generate numbers")
 area1.legend()
 plt.xlabel("number of iterations ")
 plt.ylabel("execution time")
